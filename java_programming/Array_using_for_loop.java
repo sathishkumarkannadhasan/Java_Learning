0b7b70bb80ece928ec6660bb089b2399450ef0bb
sathishkumarkannadhasan <sathishkumar252005@gmail.com>
2024-03-05 21:53:31 +0530

    New 1 file added

---
+++ java_programming/Array_using_for_loop.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    cars [3]= "Audi";
+for (int i =0; i<=3; i++){
+  System.out.println(cars[i]);
+  System.out.println(i);
+  }
+  }
+}
